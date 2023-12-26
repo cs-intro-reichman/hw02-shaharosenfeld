1bbf76abc8c8e4a62d41ffc96dd5f1387fbe57a7
shahar rosenfeld <shahar.rosenfeld@runi.ac.il>
2023-12-26 19:50:57 +0200

    (:

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -13,8 +13,71 @@ public class OneOfEachStats {
 		int T = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
-        Random generator = new Random(seed);  
-		
+        Random generator = new Random(seed);
+		double boyGirl = generator.nextDouble();  
+		boolean check = true;
+		boolean boy = false;
+		boolean girl = false;
+		int kidCounter = 0;
+		//variable above from last code
+		double avgSum =0;
+		int twoKid=0;
+		int threeKid=0;
+		int fourOrMore=0;
+		//int T = Integer.parseInt(args[0]);
+		for(int i=0; i<T; i++)//simulates familys T times
+		{
+			while(check)//family simulation
+			{
+				kidCounter++;
+				if(boyGirl == 0)
+				{
+					girl = true;
+				}
+				if(boyGirl == 1)
+				{
+					boy = true;
+				}
+				if(boy && girl)//exit if you have both
+				{
+					check = false;
+				}
+				boyGirl = generator.nextDouble();
+			}
+			avgSum = avgSum + kidCounter;
+			if(kidCounter == 2)//check how many kids it took
+			{
+				twoKid++;
+			}
+			else if(kidCounter == 3)
+			{
+				threeKid++;
+			}
+			else
+			{
+				fourOrMore++;
+			}
+			kidCounter = 0;
+			check = true;
+			girl = false;
+			boy = false;
+		}
+		avgSum = avgSum/(twoKid+threeKid+fourOrMore);
+		System.out.println("Average: "+avgSum+" children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: "+twoKid);
+		System.out.println("Number of families with 3 children: "+threeKid);
+		System.out.println("Number of families with 4 or more children: "+fourOrMore);
+		if(twoKid>=threeKid && twoKid>=fourOrMore)
+		{
+			System.out.println("The most common number of children is 2.");
+		}
+		else if(threeKid>fourOrMore)
+		{
+			System.out.println("The most common number of children is 3.");
+		}
+		else{
+			System.out.println("The most common number of children is 4.");
+		}
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
 		//// Where "rnd" is the variable that stores the generated random value.
