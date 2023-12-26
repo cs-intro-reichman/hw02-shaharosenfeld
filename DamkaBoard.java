55aa109de0464ccc602a8885da43c6f3871f06b9
shahar rosenfeld <shahar.rosenfeld@runi.ac.il>
2023-12-26 18:49:00 +0200

    (:

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -7,22 +7,21 @@ public class DamkaBoard {
 		int k =1;
 		for(int i =0; i<n; i++)
 		{
-			for(int j=0; j<n; j++)
-			{
-				/*if(j<n-1)
-				{
-					System.out.print("* ");
-				}
-				else{
-					System.out.print("*");
-				}*/
-				System.out.print("* ");	
-			}
-			System.out.println("");
 			if(k%2!=0)
 			{
-				System.out.print(" ");//every second raw starts with blank
+				for(int j=0; j<n; j++)
+				{
+				System.out.print("* ");	
+				}
 			}
+			else
+			{
+				for(int j=0; j<n; j++)
+				{
+				System.out.print(" *");	
+				}
+			}
+			System.out.println("");
 			k++;
 		}
 	}
