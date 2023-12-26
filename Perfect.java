dfcddab3396e760ee6cae4fcb1e151a1c7f27673
shahar rosenfeld <shahar.rosenfeld@runi.ac.il>
2023-12-26 15:45:48 +0200

    (:

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,24 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		int sum=1;
+		String add = " 1";
+		for(int i =2; i<num; i++)//check the sum of the dividing numbers
+		{
+			if(num%i == 0)
+			{
+				sum += i;
+				add = add + " + " + i;
+			}
+		}
+		if(sum == num)
+		{
+			System.out.println(num + " is a perfect number since "+ num + " =" + add);
+		}
+		else
+		{
+			System.out.println(num+ " is not a perfect number");
+		}
 	}
 }
