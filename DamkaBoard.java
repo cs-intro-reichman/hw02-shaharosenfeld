ce03907e785c7e72e045611ddaba5544568c8545
shahar rosenfeld <shahar.rosenfeld@runi.ac.il>
2023-12-26 18:34:59 +0200

    (:

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -9,7 +9,13 @@ public class DamkaBoard {
 		{
 			for(int j=0; j<n; j++)
 			{
-				System.out.print("* ");
+				if(j<n-1)
+				{
+					System.out.print("* ");
+				}
+				else{
+					System.out.print("*");
+				}
 			}
 			System.out.println("");
 			if(k%2!=0)
