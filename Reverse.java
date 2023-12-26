059374c8523d11e6e17fe88a7df0f88cc1acf469
shahar rosenfeld <shahar.rosenfeld@runi.ac.il>
2023-12-26 15:16:01 +0200

    (:

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,23 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String word = args[0];
+		int first=1;
+		int last = word.length()-1;//first and last will find the middle when they meet
+		char middle ='.';
+		int charPlace = 0;
+		for(int i=word.length() - 1; i>=0; i--)//loop that prints the string backwords
+		{
+			System.out.print(word.charAt(i));
+		}
+		System.out.println();
+		while(first < last)//loop to find the middle character
+		{
+			first++;
+			last--;
+			charPlace++;
+		}
+		middle = word.charAt(charPlace);
+		System.out.println("The middle character is "+middle);
 	}
 }
